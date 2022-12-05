355f4908d683278d8eab84af330747c6c14debaf
George <gwmkwan@gmail.com>
2022-12-05 08:53:56 -0700

    feat: add user badge

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -56,6 +56,17 @@ function User() {
               </div>
             </div>
           </div>
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                {name}
+                <div className="ml-2 mr-1 badge badge-success">{type}</div>
+                {hireable && (
+                  <div className="mx-1 badge badge-info">Hireable</div>
+                )}
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
     </>
