4c0ece00838ebd55689381d7a20bf9e19c7dbdd6
George <gwmkwan@gmail.com>
2022-12-05 08:46:46 -0700

    feat: add profile image into user page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,12 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import { useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import Spinner from '../components/layout/Spinner';
 import GithubContext from '../context/github/GithubContext';
 
 function User() {
-  const { getUser, user } = useContext(GithubContext);
+  const { getUser, user, loading } = useContext(GithubContext);
 
   const params = useParams();
 
@@ -12,7 +15,47 @@ function User() {
     // getUserRepos(params.login);
   }, []);
 
-  return <div>{user.login}</div>;
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to="/" className="btn btn-ghost">
+            Back To Search
+          </Link>
+        </div>
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="round-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default User;
