fd36d3b171c28d48b9a4778ee082c5a253cc6e60
George <gwmkwan@gmail.com>
2022-12-07 08:44:22 -0700

    feat: combine user and repos into one case

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,4 +1,4 @@
-const githubReducer = (state, action) => {
+const githubReducer = function (state, action) {
   switch (action.type) {
     case 'GET_USERS':
       return {
@@ -6,16 +6,11 @@ const githubReducer = (state, action) => {
         users: action.payload,
         loading: false,
       };
-    case 'GET_USER':
+    case 'GET_USER_AND_REPOS':
       return {
         ...state,
-        user: action.payload,
-        loading: false,
-      };
-    case 'GET_REPOS':
-      return {
-        ...state,
-        repos: action.payload,
+        user: action.payload.user,
+        repos: action.payload.repos,
         loading: false,
       };
     case 'SET_LOADING':
@@ -28,8 +23,10 @@ const githubReducer = (state, action) => {
         ...state,
         users: [],
       };
+
     default:
       return state;
   }
 };
+
 export default githubReducer;
