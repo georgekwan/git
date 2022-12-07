dcd6ec564f2d7bf227531ed8b9d21bfd24b31e9f
George <gwmkwan@gmail.com>
2022-12-07 11:16:02 -0700

    feat: change search function with axios

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,11 @@
+import axios from 'axios';
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+});
 
 // Get search results
 export const searchUsers = async (text) => {
@@ -7,13 +13,8 @@ export const searchUsers = async (text) => {
     q: text,
   });
 
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    headers: { Authorization: `token ${GITHUB_TOKEN}` },
-  });
-
-  const { items } = await response.json();
-
-  return items;
+  const response = await github.get(`/search/users?${params}`);
+  return response.data.items;
 };
 
 // Get single user
